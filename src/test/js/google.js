2fa821a4a18d2a20bb86b71d7fee0e83ab59836a
Will Warren <wwarren@wonderant.com>
2014-07-17 12:01:43 -0400

    google crawl warning

--- src/test/js/google.js
+++ src/test/js/google.js
@@ -1,3 +1,7 @@
+/**
+ * We highly recommend you don't crawl google as they will block your 
+ * IP and then you'll be sad.
+ */
 var urlIterator = new UrlIterator(function(pIndex){
     if (pIndex < 1) {
 		return "https://www.google.com";
