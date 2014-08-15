2a42e2e9385c174b6e3f88a704338c4501220348
Will Warren <wwarren@wonderant.com>
2014-08-15 19:47:52 -0400

    removed weird cache files

--- build.sh
+++ build.sh
@@ -7,6 +7,5 @@ mkdir -p scrapie-$version
 cp target/scrapie-*.jar scrapie-$version/scrapie.jar
 cp src/main/bin/* scrapie-$version
 cp src/test/js/google.js scrapie-$version/google.js
-cp -r src/test/groceryGatewayFiles scrapie-$version/.google.js_data
 mvn --quiet exec:java -Dexec.mainClass="com.wp.scrapie.GenerateReferenceDocs"
 zip -qr scrapie-latest.zip scrapie-$version
